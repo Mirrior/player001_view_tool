c0e3c6bf090db225aa05d03fe4947281dc841701
Rustin Player <rjplayer@outlook.com>
2018-01-31 14:54:24 -0700

    Player001 tool initial commit

---
+++ lib/player001_view_tool.rb
@@ -0,0 +1,6 @@
+require "player001_view_tool/version"
+require "player001_view_tool/renderer"
+
+module Player001ViewTool
+  # Your code goes here...
+end
---
+++ spec/player001_view_tool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Player001ViewTool do
+  it "has a version number" do
+    expect(Player001ViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
